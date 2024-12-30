55a53ea3097474bf409d6d2eff4ec83490a6d63a
munibaweb123 <munibavirtualuni@gmail.com>
2024-12-31 03:04:04 +0500

    environment variable on client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,12 +1,8 @@
 import { createClient } from '@sanity/client';
 
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-31';
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion, // Ensure this is used here
-  useCdn: true, // Use CDN for faster response times
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-01-01',
+  useCdn: true,
 });
